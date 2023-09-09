c354c8be4b754fddbcd1dada201448c64892f1dd
Freddy <fmf2005@gmail.com>
2023-09-08 20:55:52 -0400

    avanzando....

--- futbol/admin.py
+++ futbol/admin.py
@@ -4,6 +4,29 @@ from futbol.models import Player, Schedule, Team
 
 # Register your models here.
 
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(Schedule)
+# admin.site.register(Team)
+# admin.site.register(Player)
+
+
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 3
+
+
+class TeamAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     (None, {"fields": ["question_text"]}),
+    #     ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    # ]
+    list_display = ["name", "grade"]
+    inlines = [PlayerInline]
+
+
+admin.site.register(Team, TeamAdmin)
+
+
+class Scheduledmin(admin.ModelAdmin):
+    list_display = ["stages", "hc", "hc_gol", "status", "vs_gol", "vs", "date" ]
+
+admin.site.register(Schedule, Scheduledmin)
--- futbol/models.py
+++ futbol/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 
+# Teams
 class Team(models.Model):
-    name = models.CharField(max_length=100, unique=True, default="No Name")
+    name = models.CharField(max_length=100, unique=True, default="Nombre")
     grade = models.CharField(max_length=50, null=True)
+    abbrev = models.CharField(max_length=3, null=True)
 
     def __str__(self):
         return self.name
 
 
+# PLayers
 class Player(models.Model):
-    name = models.CharField(max_length=100, unique=True, default="No Name")
+    name = models.CharField(max_length=100, unique=True, default="Nombre")
     grade = models.CharField(max_length=50, null=True)
 
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
@@ -19,15 +22,23 @@ class Player(models.Model):
         return self.name
 
 
+# Schedules
 class Schedule(models.Model):
     STATUS_CHOICES = [
-        ('completed', 'Completed'),
-        ('scheduled', 'Scheduled'),
-        ('raining', 'Raining'),
-        ('playing', 'Playing'),
-        ('pending', 'Pending'),
+        ("completed", "Completed"),
+        ("scheduled", "Scheduled"),
+        ("raining", "Raining"),
+        ("playing", "Playing"),
+        ("pending", "Pending"),
     ]
-      
+    STAGES_CHOICES = [
+        ("regular", "Regular"),
+        ("octavos", "Octavos"),
+        ("cuartos", "Cuartos"),
+        ("semifinal", "SemiFinal"),
+        ("final", "Final"),
+    ]
+
     vs = models.ForeignKey(
         Team, on_delete=models.SET_NULL, related_name="vs_schedule", null=True
     )
@@ -38,5 +49,10 @@ class Schedule(models.Model):
     vs_gol = models.IntegerField(null=True)
     hc_gol = models.IntegerField(null=True)
 
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='scheduled')
-    
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="scheduled"
+    )
+
+    stages = models.CharField(
+        max_length=20, choices=STAGES_CHOICES, default="regular"
+    )
