ea98a5446a320d809aee657cc59ca21a2d2407fd
Freddy <fmf2005@gmail.com>
2023-09-29 19:44:19 -0400

    dev: primeras imagenes de atletas

--- futbol/models/player.py
+++ futbol/models/player.py
@@ -8,7 +8,7 @@ class Player(models.Model):
     grade = models.CharField(max_length=50, null=True, blank=True)
 
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
-    imagen = models.CharField(max_length=3, null=True, blank=True)
+    imagen = models.CharField(max_length=100, null=True, blank=True)
     alias = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
