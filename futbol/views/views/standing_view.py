8bd3d8031944f4a938c62a014eccc3daa59cb8e9
Freddy <fmf2005@gmail.com>
2024-02-29 20:13:16 -0500

    dev: reparando stadings

--- futbol/views/views/standing_view.py
+++ futbol/views/views/standing_view.py
@@ -14,24 +14,10 @@ class StandingView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        standings = Standing.objects.all()
-        # if '_from' in kwargs and '_to' in kwargs:
-        #     _from = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_from']  
-        #     _to = date.today().strftime("%Y-%m-%d") if kwargs['_to'] == 'today' else kwargs['_to']
-        #     schedules = Schedule.objects.filter(date__range=[_from, _to])
-        # elif '_from' in kwargs: 
-        #     _from = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_from']  
-        #     schedules = Schedule.objects.filter(date__gte=_from)
-        # elif '_to' in kwargs: 
-        #     _to = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_to']  
-        #     schedules = Schedule.objects.filter( date__lte=_to)
-
-            
-        # if 'team_slug' in kwargs:
-        #     team = Team.objects.get(slug=kwargs['team_slug'])
-        #     schedules = schedules.filter(Q(vs=team) | Q(hc=team))
-        #     context['team'] = team
-
+        league = League.objects.get(slug=kwargs.get('league_slug'))
+        
+        standings = standings = Standing.objects.filter(league=league)
+      
         if 'league_slug' in kwargs:
             league = League.objects.get(slug=kwargs.get('league_slug'))
             # schedules = schedules.filter(league__slug=kwargs.get('league_slug'))
@@ -49,3 +35,21 @@ class StandingView(TemplateView):
         context['standings'] = standings
 
         return context
+
+
+  # if '_from' in kwargs and '_to' in kwargs:
+        #     _from = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_from']  
+        #     _to = date.today().strftime("%Y-%m-%d") if kwargs['_to'] == 'today' else kwargs['_to']
+        #     schedules = Schedule.objects.filter(date__range=[_from, _to])
+        # elif '_from' in kwargs: 
+        #     _from = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_from']  
+        #     schedules = Schedule.objects.filter(date__gte=_from)
+        # elif '_to' in kwargs: 
+        #     _to = date.today().strftime("%Y-%m-%d") if kwargs['_from'] == 'today' else kwargs['_to']  
+        #     schedules = Schedule.objects.filter( date__lte=_to)
+
+            
+        # if 'team_slug' in kwargs:
+        #     team = Team.objects.get(slug=kwargs['team_slug'])
+        #     schedules = schedules.filter(Q(vs=team) | Q(hc=team))
+        #     context['team'] = team
